0964cf20c9419134628456734c00a50fc1977a3c
Maxwell DeMers <maxuuell@gmail.com>
2019-04-09 21:44:45 -0700

    Remove flipp toolkit reference for failed build

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import { Flipper, Flipped } from "react-flip-toolkit"
 
 import Button from "./Button"
 import MobileMenuIcon from "./MobileMenuIcon"
