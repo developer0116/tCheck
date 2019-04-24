8e6799dc325326067f33490ab7c76584de2fb2b2
Maxwell DeMers <maxuuell@gmail.com>
2019-04-23 19:54:28 -0700

    Fix fonts in hero of Processors page

--- src/components/CustomerSegmentHero.js
+++ src/components/CustomerSegmentHero.js
@@ -6,6 +6,7 @@ import BackgroundImage from "gatsby-background-image"
 import { Header4, Lead } from "./Typography"
 
 import { Flour } from "../lib/colors"
+import { sizes } from "../lib/layout"
 
 // const CustomerSegmentHeroBackground = styled(BackgroundImage)`
 //   height: 30rem;
@@ -15,7 +16,7 @@ const CustomerSegmentHeroBackground = styled.div`
   background: url(${({ heroImgURL }) => {
     return heroImgURL
   }});
-  height: 30rem;
+  height: 28rem;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: bottom !important;
@@ -26,15 +27,47 @@ const CustomerSegmentHeroBackground = styled.div`
 const CustomerSegmentContentContainer = styled.div`
   margin: 0 0 0 6.5rem;
   color: ${Flour};
+
+  @media (max-width: ${sizes.modifiedTablet}) and (min-width: ${sizes.mobileL}) {
+    margin: 0 0 0 2rem;
+  }
+
+  @media (max-width: ${sizes.mobileL}) and (min-width: ${sizes.mobileS}) {
+    text-align: center;
+    margin: 0 auto;
+  }
 `
 
 const Header = styled(Header4)`
   margin: 0 0 2rem 0;
+
+  @media (max-width: ${sizes.modifiedTablet}) and (min-width: ${sizes.mobileL}) {
+    font-size: 1.5rem;
+    margin: 0 0 1rem 0;
+  }
+
+  @media (max-width: ${sizes.mobileL}) and (min-width: ${sizes.mobileS}) {
+    font-size: 1.5rem;
+    text-align: center;
+    margin: 0 auto 1rem;
+    width: 80%;
+  }
 `
 
 const HeroLead = styled(Lead)`
   font-size: 1.25rem;
   max-width: 35rem;
+
+  @media (max-width: ${sizes.modifiedTablet}) and (min-width: ${sizes.mobileL}) {
+    font-size: 1rem;
+  }
+
+  @media (max-width: ${sizes.mobileL}) and (min-width: ${sizes.mobileS}) {
+    font-size: 1rem;
+    text-align: center;
+    margin: 0 auto;
+    width: 85%;
+  }
 `
 
 const CustomerSegmentHero = React.memo(
