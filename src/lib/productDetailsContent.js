002422930e9787d24116b85f8ad50d2b2498997d
Maxwell DeMers <maxuuell@gmail.com>
2019-04-29 19:05:27 -0700

    Add remaining content for product page

--- src/lib/productDetailsContent.js
+++ src/lib/productDetailsContent.js
@@ -107,18 +107,128 @@ const productDetailsContent = {
     ],
   },
   2: {
-    description: `It's a thing tCheck`,
-    features: `Feature Content tCheck`,
-    included: `Whats Included?!?!?! tCheck`,
-    shipping: `Ship Shape tCheck`,
-    specs: `Spectacular tCheck`,
+    features: {
+      paragraphsArray: [
+        `tCheck 2 is a potency tester that allows you to measure the strength of your infusions. Using just a few drops of butter, coconut oil, olive oil, or alcohol, you can accurately measure the strength of your infusion in less than 45 seconds.`,
+        `Results from tCheck 2 will be an aggregate of all compounds combined.`,
+      ],
+      listItemsArray: [
+        `Ultra Violet spectrometer`,
+        `Reusable sample tray`,
+        `Test infusions, get results and dose recipes`,
+        `Battery operated`,
+        `Results in CBV`,
+        `UL, CE, FCC certified`,
+        `Bluetooth LE`,
+      ],
+    },
+    included: {
+      unorderedListsArray: [
+        {
+          title: ``,
+          listItems: [
+            `tCheck device`,
+            `Reusable testing tray`,
+            `Eye Dropper`,
+            `3 AAA batteries`,
+            `Quick start guide`,
+          ],
+        },
+      ],
+    },
+
+    shipping: [
+      `Before we ship, every tCheck 2 and the kit is carefully tested.`,
+      `Ships in 2-3 weeks. At this time, tCheck 2 is not shipping into any EU nation.`,
+    ],
+    specs: [
+      {
+        title: `BREAKDOWN`,
+        textArray: [
+          `tCheck 2 tests: Compound by Volume (CBV), THC, CBD, or THCa (with expansion kit)`,
+        ],
+      },
+      {
+        title: `ACCURACY`,
+        textArray: [`+/- 15% Decarbed material`],
+      },
+      {
+        title: `OPTIMAL TEMPERATURE`,
+        textArray: [`50 ~ 100° F`, `(10 ~ 38° C)`],
+      },
+      {
+        title: `APP SUPPORT`,
+        textArray: [
+          `Required: iOS 11+ and Android 5.0+ with Bluetooth LE (4.0) support`,
+        ],
+      },
+      {
+        title: `MEASUREMENT RANGES`,
+        textArray: [
+          `Olive Oil: 0 ~ 15 mg/ml`,
+          `Butter/Ghee: 0 ~ 15 mg/ml`,
+          `Coconut/MCT Oil: 0 ~ 15 mg/ml`,
+          `Alcohol Tinctures: 0 ~ 15 mg/ml`,
+        ],
+      },
+      {
+        title: `BATTERY`,
+        textArray: [
+          `3 AAA Alkaline, Lithium, NiMH`,
+          `DO NOT USE NiCd batteries in t2`,
+          `Batteries should last 4-6 months with average use`,
+        ],
+      },
+      {
+        title: `REGULATORY`,
+        textArray: [`UL Listed, CE Mark, and FCC compliant`],
+      },
+    ],
   },
   3: {
-    description: `It's a thing tCheck`,
-    features: `Feature Content tCheck`,
-    included: `Whats Included?!?!?! tCheck`,
-    shipping: `Ship Shape tCheck`,
-    specs: `Spectacular tCheck`,
+    features: {
+      paragraphsArray: [
+        `The expansion kit provides all the materials needed to properly test your flower and concentrates using your tCheck 2. Each item included has been carefully selected to ensure the highest level of testing accuracy with tCheck.`,
+      ],
+      listItemsArray: [
+        `Ability to run 9 flower or concentrate tests with 100ml bottle of reagent`,
+      ],
+    },
+    included: {
+      unorderedListsArray: [
+        {
+          title: ``,
+          listItems: [
+            `Scale`,
+            `100ml reagent`,
+            `Eye dropper`,
+            `Swizzle stick`,
+            `Syringe`,
+            `3 spoons`,
+            `3 containers`,
+            `Quick start guide`,
+          ],
+        },
+      ],
+    },
+    shipping: [
+      `Before we ship, every tCheck 2 and the kit is carefully tested.`,
+      `Ships in 2-3 weeks. At this time, tCheck 2 is not shipping into any EU nation.`,
+    ],
+    specs: [
+      {
+        title: `TESTS`,
+        textArray: [`Flower and all concentrate derivatives`],
+      },
+      {
+        title: `KIT REQUIREMENTS`,
+        textArray: [
+          `80-120mg of flower material`,
+          `10-15mg of concentrate material`,
+          `10ml of t2 Special Reagent`,
+        ],
+      },
+    ],
   },
 }
 
