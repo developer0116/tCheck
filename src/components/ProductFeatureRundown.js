9179b6eb9880dedd06abe18810801c663a05eacc
Maxwell DeMers <maxuuell@gmail.com>
2019-05-09 21:15:29 -0700

    Add Feature Table

--- src/components/ProductFeatureRundown.js
+++ src/components/ProductFeatureRundown.js
@@ -3,84 +3,193 @@ import styled from "styled-components"
 
 import { Body, Header5 } from "./Typography"
 
-import { Fade, Ghost } from "../lib/colors"
+import { Dada } from "../lib/colors"
 import { sizes } from "../lib/layout"
 
 import CheckMark from "../images/greenCheckMark.svg"
 
-const RundownContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: repeat(14, 1fr);
-  grid-auto-flow: column;
-  align-items: center;
-  margin: 2rem auto 0;
-  max-width: 45rem;
+const Container = styled.section`
+  margin: 3.5rem auto;
+  width: 100%;
+  max-width: 50rem;
+`
+
+const Header = styled(Header5)`
+  @media (max-width: ${sizes.modifiedTablet}) and (min-width: ${sizes.mobileS}) {
+    text-align: center;
+  }
+`
+
+// Apparantly, setting a border on a tr requires this
+// https://stackoverflow.com/questions/20872200/giving-a-border-to-an-html-table-row-tr
+const TableContainer = styled.table`
+  margin: 2rem 0;
+  width: 100%;
+  border-collapse: collapse;
+`
+
+const TableRow = styled.tr`
+  border-bottom: 1px solid ${Dada};
+`
+
+const TableData = styled.td`
+  text-align: center;
 `
 
 const ColumnTitle = styled(Body)`
   text-align: center;
+  padding: 1rem;
 `
 
 const FeatureItemTitle = styled(Body)`
-  margin: 0 0 0 1rem;
+  text-align: left;
+  padding: 1rem;
 `
 
 const RowText = styled(Body)`
+  padding: 1rem;
+
   text-align: center;
 `
 
 const GreenCheckMark = styled.img`
+  height: 1rem;
+  width: 1rem;
   margin: 0 auto;
 `
 
 const ProductFeatureRundown = React.memo(() => {
   return (
-    <RundownContainer>
-      <Header5>Feature Rundown</Header5>
-      <span />
-      <span />
-      <FeatureItemTitle secondary>Price Base</FeatureItemTitle>
-      <FeatureItemTitle secondary>CBV measurement</FeatureItemTitle>
-      <FeatureItemTitle secondary>Coconut oil</FeatureItemTitle>
-      <FeatureItemTitle secondary>Butter</FeatureItemTitle>
-      <FeatureItemTitle secondary>Olive Oil</FeatureItemTitle>
-      <FeatureItemTitle secondary>Alcohol</FeatureItemTitle>
-      <FeatureItemTitle secondary>iOS & Android app</FeatureItemTitle>
-      <FeatureItemTitle secondary>History Log</FeatureItemTitle>
-      <FeatureItemTitle secondary>Plant material</FeatureItemTitle>
-      <FeatureItemTitle secondary>Concentrates</FeatureItemTitle>
-      <FeatureItemTitle secondary>6 month warranty</FeatureItemTitle>
-      <span />
-      <ColumnTitle>tCheck 2 Device</ColumnTitle>
-      <span />
-      <RowText secondary>$279.99</RowText>
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <span />
-      <span />
-      <GreenCheckMark src={CheckMark} />
-      <span />
-      <ColumnTitle>Expansion Kit</ColumnTitle>
-      <RowText secondary>Requires tCheck 2 device</RowText>
-      <span />
-      <RowText secondary>$199.99</RowText>
-      <GreenCheckMark src={CheckMark} />
-      <span />
-      <span />
-      <span />
-      <span />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <GreenCheckMark src={CheckMark} />
-      <span />
-    </RundownContainer>
+    <Container>
+      <Header>Feature Rundown</Header>
+      <TableContainer>
+        <tbody>
+          <TableRow>
+            <th />
+            <TableData>
+              <ColumnTitle>tCheck 2 Device</ColumnTitle>
+            </TableData>
+            <TableData>
+              <ColumnTitle>Expansion Kit</ColumnTitle>
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th />
+            <TableData />
+            <TableData>
+              <RowText secondary>Requires tCheck 2 device</RowText>
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>Price Base</FeatureItemTitle>
+            </th>
+            <TableData>
+              <RowText secondary>$279.99</RowText>
+            </TableData>
+            <TableData>
+              <RowText secondary>$199.99</RowText>
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>CBV measurement</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>Coconut oil</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData />
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>Butter</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData />
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>Olive Oil</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData />
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>Alcohol</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData />
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>iOS & Android app</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>History Log</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>Plant Material</FeatureItemTitle>
+            </th>
+            <TableData />
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>Concentrates</FeatureItemTitle>
+            </th>
+            <TableData />
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+          </TableRow>
+          <TableRow>
+            <th>
+              <FeatureItemTitle secondary>6 month warranty</FeatureItemTitle>
+            </th>
+            <TableData>
+              <GreenCheckMark src={CheckMark} />
+            </TableData>
+            <TableData />
+          </TableRow>
+        </tbody>
+      </TableContainer>
+    </Container>
   )
 })
 
